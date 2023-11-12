b5ecfcead9f22a21eacfed990a62bcf95f2e1697
Bartas Dausynas <bartas3@gmail.com>
2023-11-12 18:25:24 +0200

    test done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-const meh 
+const meh = document.getElementById("button");
+const login = document.querySelector(".login")
+const gameboard = document.querySelector(".gameboard");
+gameboard.classList.add('invisible');
+
+function changeDOM()
+{
+    gameboard.classList.remove('invisible');
+    login.classList.add('invisible');
+}
+
+meh.addEventListener('click', changeDOM);
